2f2a806243c978d6dd99ce0ca3049109bed578ec
swordcube <49110074+swordcube@users.noreply.github.com>
2025-06-01 19:29:39 -0500

    add --depth 1 to haxelib git cmd

--- src/hmm/utils/Shell.hx
+++ src/hmm/utils/Shell.hx
@@ -94,7 +94,7 @@ class Shell {
   }
 
   public static function haxelibGit(name : String, url : String, ref : Option<String>, dir : Option<String>, options: ShellOptions) : Void {
-    var args = ["git", name, url, "--skip-dependencies"].concat(ref.toArray()).concat(dir.toArray());
+    var args = ["git", name, url, "--skip-dependencies", "--depth", "1"].concat(ref.toArray()).concat(dir.toArray());
     return haxelib(args, options);
   }
 
